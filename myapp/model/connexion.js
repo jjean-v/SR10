78bae25186aa0f470171b2893ccbd6564bc5d350
Lewis Botokeky <lewis.botokeky@etu.utc.fr>
2025-03-28 10:52:09 +0100

    ajout du dossier assets pour le html et css

---
+++ myapp/model/connexion.js
@@ -0,0 +1,10 @@
+var mysql = require("mysql");
+var pool = mysql.createPool({
+host: "tuxa.sme.utc", //ou localhost
+user: "sr10p010"
+,
+password: "GtsU8cu10zjG"
+,
+database: "sr10p010"
+});
+module.exports = pool;
