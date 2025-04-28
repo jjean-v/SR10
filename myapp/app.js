a16e9a3daffb291e56de825611778f1db38a6148
jjean-v <14jean04@gmail.com>
2025-04-28 15:53:32 +0200

    creation d'un compte

--- myapp/app.js
+++ myapp/app.js
@@ -20,9 +20,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(logger('dev')); 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,6 +36,7 @@ app.use('/organisations',organisationRouter)
 app.use('/fiche_de_poste',fiche_de_posteRouter)
 app.use('/offre', offreRouter)
 
+app.use('/creation_compte',new_userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- myapp/routes/new_user.js
+++ myapp/routes/new_user.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const utilisateur = require('../model/utilisateur');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('new_user');
+});
+
+router.post('/', function(req, res, next) {
+  const { nom, prenom, password, email, zip } = req.body;
+  promise = utilisateur.create({nom ,prenom, email , motDePasse:password, role:'candidat',role_recruteur: null,etat_compte:'alive',siren:null})
+  promise.then( (data) =>{
+
+    res.render('creation_compte', { title: 'creation du compte', user: data });
+
+  });
+
+  promise.catch( (err) => {
+      console.log(err);
+      res.status(500).send('Error retrieving new user data');
+  }); 
+
 });
 
 module.exports = router;
