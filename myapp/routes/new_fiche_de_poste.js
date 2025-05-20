2ec3abd4db09889946ee2c9035135922c88d1fbb
Lewis Botokeky <lewis.botokeky@etu.utc.fr>
2025-05-20 10:10:56 +0200

    formulaire fiche de poste

---
+++ myapp/routes/new_fiche_de_poste.js
@@ -0,0 +1,32 @@
+var express = require('express');
+const ficheDePoste = require('../model/ficheDePoste');
+
+var router = express.Router();
+
+/* GET new fiche de poste page. */
+router.get('/', function(req, res, next) {
+    res.render('new_fiche_de_poste');
+});
+
+router.post('/', function(req, res, next) {
+    const { titre, description, salaire, localisation, typeContrat } = req.body;
+    const promise = ficheDePoste.create({
+        titre,
+        description,
+        salaire,
+        localisation,
+        typeContrat,
+        dateCreation: new Date()
+    });
+
+    promise.then((data) => {
+        res.render('fiche_de_poste_cree', { title: 'Fiche de poste créée', fiche: data });
+    });
+
+    promise.catch((err) => {
+        console.log(err);
+        res.status(500).send('Erreur lors de la création de la fiche de poste');
+    });
+});
+
+module.exports = router;
